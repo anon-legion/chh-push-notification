7b72374267d0d982e4ed3ca2acddb20f180d1962
=GV= <g.sync.mobile1@gmail.com>
2023-10-25 17:34:50 +0800

    update type definition of xss-sanitizer middleware

--- middlewares/xss-sanitzer.ts
+++ middlewares/xss-sanitzer.ts
@@ -1,11 +1,11 @@
 import xss from 'xss';
 import type { Request, Response, NextFunction } from 'express';
 
+type ValidData = Request['body'] | Request['query'] | Request['params'];
 type ValidKeys = 'body' | 'params' | 'query';
 type KeyArray = ValidKeys[];
 
-const clean = (data: any): any => {
-  console.log(data);
+function clean<T extends ValidData>(data: ValidData): T {
   let isObject = false;
   if (typeof data === 'object') {
     data = JSON.stringify(data);
@@ -16,13 +16,13 @@ const clean = (data: any): any => {
   if (isObject) data = JSON.parse(data);
 
   return data;
-};
+}
 
 function xssSanitizer(keys: KeyArray = []) {
   return (req: Request, _res: Response, next: NextFunction) => {
-    if (req.body && (keys.includes('body') || !keys.length)) req.body = clean(req.body);
-    if (req.query && (keys.includes('query') || !keys.length)) req.query = clean(req.query);
-    if (req.params && (keys.includes('params') || !keys.length)) req.params = clean(req.params);
+    if (req.body && (keys.includes('body') || !keys.length)) req.body = clean<Request['body']>(req.body);
+    if (req.query && (keys.includes('query') || !keys.length)) req.query = clean<Request['query']>(req.query);
+    if (req.params && (keys.includes('params') || !keys.length)) req.params = clean<Request['params']>(req.params);
 
     next();
   };
