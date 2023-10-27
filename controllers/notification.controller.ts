28edd44d368bfd3e2b8821c83a75c779157b7657
=GV= <g.sync.mobile1@gmail.com>
2023-10-27 15:18:56 +0800

    update random notificaiton controller to generate randomized content

--- controllers/notification.controller.ts
+++ controllers/notification.controller.ts
@@ -4,28 +4,55 @@ import Notification from '../models/Notification';
 import { InternalServerError } from '../errors';
 import resObj from './utilities/success-response';
 
+const randomAppReceiver = (): string => {
+  const apps = ['doki', 'nursi', 'pxi', 'resi'];
+  return apps[Math.floor(Math.random() * apps.length)];
+};
+
+const randomMessage = (): string => {
+  const messages = [
+    'lorem ipsum dolor sit amet',
+    'consectetur adipiscing elit',
+    'sed do eiusmod tempor',
+    'incididunt ut labore',
+    'et dolore magna aliqua',
+    'ut enim ad minim veniam',
+  ];
+
+  return messages[Math.floor(Math.random() * messages.length)];
+};
+
+const randomMessageType = (): string => {
+  const types = ['admission', 'approve', 'diagResults', 'pf'];
+  return types[Math.floor(Math.random() * types.length)];
+};
+
+const randomRecipientId = (): string => {
+  return String(Math.floor(Math.random() * 1000000000000));
+};
+
 async function populateNotificaiton(_req: Request, res: Response) {
   try {
     const [notif1, notif2, notif3] = await Promise.all([
       Notification.create({
-        appReceiver: 'doki',
-        message: 'Notification for doki',
-        messageType: 'pf',
-        recipientId: String(Math.floor(Math.random() * 1000000000000)),
+        appReceiver: randomAppReceiver(),
+        message: randomMessage(),
+        messageType: randomMessageType(),
+        recipientId: randomRecipientId(),
         status: 1,
       }),
       Notification.create({
-        appReceiver: 'resi',
-        message: 'Notification for resi',
-        messageType: 'approve',
-        recipientId: String(Math.floor(Math.random() * 1000000000000)),
+        appReceiver: randomAppReceiver(),
+        message: randomMessage(),
+        messageType: randomMessageType(),
+        recipientId: randomRecipientId(),
         status: 1,
       }),
       Notification.create({
-        appReceiver: 'pxi',
-        message: 'Hello world',
-        messageType: 'diagResults',
-        recipientId: String(Math.floor(Math.random() * 1000000000000)),
+        appReceiver: randomAppReceiver(),
+        message: randomMessage(),
+        messageType: randomMessageType(),
+        recipientId: randomRecipientId(),
         status: 1,
       }),
     ]);
